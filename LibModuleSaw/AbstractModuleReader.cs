8a6f0e71908ad3f33762caf6310c2b4ff080d4b9
Katelyn Gadd <kg@luminance.org>
2017-12-13 19:34:51 -0800

    Checkpoint

--- LibModuleSaw/AbstractModuleReader.cs
+++ LibModuleSaw/AbstractModuleReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,11 +14,47 @@ namespace ModuleSaw {
             public long   Length;
         }
 
+        public class StreamList : IEnumerable<StreamHeader> {
+            internal StreamHeader[] Headers;
+            internal Dictionary<string, StreamHeader> Table =
+                new Dictionary<string, StreamHeader>(StringComparer.Ordinal);
+
+            internal StreamList () {
+            }
+
+            public StreamHeader this [int index] {
+                get {
+                    return Headers[index];
+                }
+            }
+
+            public StreamHeader this [string key] {
+                get {
+                    return Table[key];
+                }
+            }
+
+            public int Count {
+                get {
+                    return Headers.Length;
+                }
+            }
+
+            public IEnumerator<StreamHeader> GetEnumerator () {
+                return ((IEnumerable<StreamHeader>)Headers).GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator () {
+                return ((IEnumerable<StreamHeader>)Headers).GetEnumerator();
+            }
+        }
+
         public readonly BinaryReader Reader;
 
         public Configuration Configuration;
 
-        public StreamHeader[] StreamHeaders { get; private set; }
+        public readonly StreamList Streams = new StreamList();
+
         public string SubFormat { get; private set; }
 
         public AbstractModuleReader (BinaryReader input, Configuration configuration) {
@@ -29,7 +66,7 @@ namespace ModuleSaw {
             var buffer = Reader.ReadBytes(AbstractModuleBuilder.Prologue.Length);
             return buffer.SequenceEqual(AbstractModuleBuilder.Prologue);
         }
-
+        
         public bool ReadHeader () {
             if (!ReadPrologue())
                 return false;
@@ -42,17 +79,21 @@ namespace ModuleSaw {
             var streamCount = Reader.ReadInt32();
             var headers = new StreamHeader[streamCount];
             var keyBuffer = new byte[KeyedStream.MaxKeyLength];
+            Streams.Table.Clear();
+
             for (int i = 0; i < streamCount; i++) {
                 Reader.Read(keyBuffer, 0, keyBuffer.Length);
 
                 headers[i] = new StreamHeader {
-                    Key = Encoding.UTF8.GetString(keyBuffer, 0, Array.IndexOf(keyBuffer, 0)),
+                    Key = Encoding.UTF8.GetString(keyBuffer, 0, Array.IndexOf(keyBuffer, (byte)0)),
                     Offset = Reader.ReadInt64(),
                     Length = Reader.ReadInt64()
                 };
+
+                Streams.Table.Add(headers[i].Key, headers[i]);
             }
 
-            StreamHeaders = headers;
+            Streams.Headers = headers;
 
             if (Reader.ReadUInt32() != AbstractModuleBuilder.BoundaryMarker2)
                 return false;
